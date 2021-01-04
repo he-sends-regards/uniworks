bc710b62a98de33cf6d5beae30951b307e69c3e0
postscriptumno <postscriptum.no@gmail.com>
2021-01-05 01:07:58 +0200

    Добавил роуты по сайту

---
+++ client/src/Routes/routes.ts
@@ -0,0 +1,19 @@
+export const headersData: { label: string, href: string }[] = [
+  {
+    label: "Главная",
+    href: "/",
+  },
+  {
+    label: "Работы",
+    href: "/works",
+  },
+  {
+    label: "О нас",
+    href: "/about",
+  },
+  {
+    label: "Правила",
+    href: "/rules",
+  },
+];
+
