7275f7b71bb2f8ab17c48f7ba354cbbd54a70715
postscriptumno <postscriptum.no@gmail.com>
2021-01-05 14:51:12 +0200

    Рефакторинг

--- client/src/components/headerNavigation/headerNavigation.tsx
+++ client/src/components/headerNavigation/headerNavigation.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Redirect, useLocation } from 'react-router-dom';
-import logo from './img/logo.png';
-import { createBrowserHistory } from 'history';
-import { headersData } from '../../Routes/routes';
 import './style.css';
+import NavBar from './components/navBar/navBar';
 
 const HeaderNavigation = () => {
+	const [isFormShown, setFormShown] = useState(false);
+
 	const currentLocation = useLocation().pathname;
 
 	return (
@@ -13,32 +13,60 @@ const HeaderNavigation = () => {
 			<header>
 				<nav>
 					<div className="nav-wrapper purple darken-2 px1">
-						<Link to="/" className="brand-logo">
-							UniWorks
+						<Link
+							to="/"
+							className="brand-logo center"
+							style={{
+								fontFamily: 'Montserrat, sans-serif',
+								fontWeight: 400
+							}}
+						>
+							UNIWORKS
 						</Link>
 						<a href="#" data-target="mobile-demo" className="sidenav-trigger">
 							<i className="material-icons">menu</i>
 						</a>
+
+						<NavBar />
+
 						<ul className="right hide-on-med-and-down">
-							{headersData.map(({ label, href }) => (
-								<li key={`header-${label}-tab`}>
-									<Link className={`${currentLocation === href && `purple darken-3`}`} to={href}>
-										{label}
-									</Link>
-								</li>
-							))}
+							<li>
+								<Link
+									className={`waves-effect ${
+										currentLocation === '/auth/login'
+											? `waves-light purple darken-3`
+											: `waves-purple white`
+									} btn`}
+									style={
+										currentLocation === '/auth/login'
+											? { color: 'white', boxShadow: '0' }
+											: { color: '#7b1fa2' }
+									}
+									to="/auth/login"
+								>
+									Login
+								</Link>
+							</li>
+							<li>
+								<Link
+									className={`waves-effect ${
+										currentLocation === '/auth/register'
+											? `waves-light purple darken-3`
+											: `waves-purple white`
+									} btn`}
+									style={
+										currentLocation === '/auth/register'
+											? { color: 'white', boxShadow: '0' }
+											: { color: '#7b1fa2' }
+									}
+									to="/auth/register"
+								>
+									Register
+								</Link>
+							</li>
 						</ul>
 					</div>
 				</nav>
-				<ul className="sidenav" id="mobile-demo">
-					{headersData.map(({ label, href }) => (
-						<li key={`header-${label}-tab`}>
-							<Link className={`${currentLocation === href && `purple darken-3`}`} to={href}>
-								{label}
-							</Link>
-						</li>
-					))}
-				</ul>
 			</header>
 		</>
 	);
