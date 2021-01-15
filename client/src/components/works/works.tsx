0ec8558a6bd57871cf9a870e834bd21a8793118b
postscriptumno <postscriptum.no@gmail.com>
2021-01-16 00:11:23 +0200

    Оставил только фильтры

--- client/src/components/works/works.tsx
+++ client/src/components/works/works.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
-import Filter from './components/filter/filter';
+import Filter from '../filter/course-filter';
 
 import './works.css';
 
 const Works = () => {
 	return (
-		<div className="works-container">
-			<Filter />
-
-			<div className="works_instance">
-				<div className="works_preview">preview</div>
-				<div className="works_download-btn">
-					<button className="btn waves-effect waves-light col s12" type="submit" name="action">
-						Download
-						<i className="material-icons right">
-							{
-								// if payed
-								true ? `file_download` : `cancel`
-							}
-						</i>
-					</button>
-				</div>
+		<div className="wrapper">
+			<div>
+				<Filter />
 			</div>
+			{/* <div className="works-container">
+				<Filter />
+
+				<div className="works_instance">
+					<div className="works_preview">preview</div>
+					<div className="works_download-btn">
+						<button className="btn waves-effect waves-light col s12" type="submit" name="action">
+							Download
+							<i className="material-icons right">
+								{
+									// if payed
+									true ? `file_download` : `cancel`
+								}
+							</i>
+						</button>
+					</div>
+				</div>
+			</div> */}
 		</div>
 	);
 };
