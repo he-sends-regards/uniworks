97f6274dd5ac836a162f658b1b68d8ad2916775d
postscriptumno <postscriptum.no@gmail.com>
2021-01-16 00:07:29 +0200

    Создал файл для констант. Добавил в него массив курсов

---
+++ client/src/const.ts
@@ -0,0 +1,17 @@
+export const courses = [
+  {
+    name: '3 курс',
+    eventKey: 'third-course',
+    disabled: false
+  },
+  {
+    name: '2 курс',
+    eventKey: 'second-course',
+    disabled: true
+  },
+  {
+    name: '1 курс',
+    eventKey: 'first-course',
+    disabled: true
+  }
+];
