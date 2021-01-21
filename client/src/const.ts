b4c4ca54389652e71bd618fd80e67baab1e0a977
postscriptumno <postscriptum.no@gmail.com>
2021-01-21 12:18:26 +0200

    Изменил структуру данных

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,37 +1,133 @@
-export const courses: {name: string, id: string, eventKey: string, disabled: boolean}[] = [
+export interface ISubject {
+  name: string;
+  id: string;
+  eventkey: string;
+  href: string;
+  data: string[];
+}
+interface ISubjects {
+  secondCourse: [];
+  firstCourse: [];
+  thirdCourse: ISubject[]
+}
+
+export const courses: {name: string, id: string, eventKey: string, disabled: boolean, subjects: ISubject[]}[] = [
   {
     name: '3 курс',
     id: 'thirdCourse',
     eventKey: 'third-course',
-    disabled: false
+    disabled: false,
+    subjects: [
+      {
+        name: 'ММДС',
+        id: 'mmds',
+        eventkey: 'mmds',
+        href: '',
+        data: [
+          'л.р.№3.1',
+          'л.р.№3.2',
+          'РГР',
+          'л.р.№4.1',
+          'л.р.№4.2',
+          'л.р.№4.3',
+          'л.р.№4.4',
+        ]
+      },
+      {
+        name: 'СА',
+        id: 'sa',
+        eventkey: 'sa',
+        href: '',
+        data: [
+          'л.р.№1',
+          'л.р.№2',
+          'л.р.№3',
+          'л.р.№1_2',
+          'ДЗ',
+          'МКР№1',
+          'МКР№2',
+        ]
+      },
+      {
+        name: 'ОТИС',
+        id: 'otis',
+        eventkey: 'otis',
+        href: '',
+        data: [
+          'л.р.№1-1',
+          'л.р.№1-2',
+          'л.р.№1-3',
+          'МКР№1',
+          'л.р.№2-1',
+          'л.р.№2-2',
+          'л.р.№2-3',
+          'л.р.№2-4',
+          'МКР№2',
+        ]
+      },
+      {
+        name: 'ММДО',
+        id: 'mmdo',
+        eventkey: 'mmdo',
+        href: '',
+        data: [
+          'л.р.№1',
+          'ДЗ'
+        ]
+      },
+      {
+        name: 'ТКП',
+        id: 'tkp',
+        eventkey: 'tkp',
+        href: '',
+        data: [
+          'л.р.№1',
+          'л.р.№2',
+          'л.р.№3',
+          'л.р.№4',
+          'л.р.№5',
+          'л.р.№6',
+          'л.р.№7',
+          'МКР№1',
+          'МКР№2'
+        ]
+      },
+      {
+        name: 'ОБДЗ',
+        id: 'obdz',
+        eventkey: 'obdz',
+        href: '',
+        data: [
+          'л.р.№1',
+          'л.р.№2',
+          'л.р.№3',
+          'л.р.№4',
+          'л.р.№5',
+          'л.р.№6',
+          'МКР№1',
+          'МКР№2',
+          'Курсовая'
+        ]
+      }
+    ]
   },
   {
     name: '2 курс',
     id: 'secondCourse',
     eventKey: 'second-course',
-    disabled: true
+    disabled: true,
+    subjects: []
   },
   {
     name: '1 курс',
     id: 'firstCourse',
     eventKey: 'first-course',
-    disabled: true
+    disabled: true,
+    subjects: []
   }
 ];
 
-interface Subjects {
-  secondCourse: [];
-  firstCourse: [];
-  thirdCourse: {
-    name: string,
-    id: string,
-    eventkey: string,
-    href: string,
-    data: string[]
-  }[]
-}
-
-export const subjects:Subjects = {
+export const subjects:ISubjects = {
   thirdCourse: [
     {
       name: 'ММДС',
