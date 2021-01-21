1d5250ffebb06de426df1cbbd2eb1ade93998ade
postscriptumno <postscriptum.no@gmail.com>
2021-01-21 12:20:10 +0200

    Написал демо окна работы

--- client/src/components/filter/work-filter/work-filter.tsx
+++ client/src/components/filter/work-filter/work-filter.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import { Tab, Row, Col, Nav } from 'react-bootstrap';
 import { SubjectWin } from '../../subject-win/subject-win';
+import { ISubject } from '../../../const';
 
 // interface WorkFilterSubjectProps {
 // 	subjectData: string[];
 // 	subjectId: string;
 // }
 
-const WorkFilter: React.FC = () => {
+type WorkType = {
+	works: string[];
+};
+
+const WorkFilter: React.FC<WorkType> = ({ works }) => {
 	// const subjectData = subjects.thirdCourse.filter(subject => subject.id === 'mmds').data;
 
 	return (
 		<Tab.Container id="left-tabs-example" defaultActiveKey="first">
-			<Row>
-				<Col sm={1}>
+			<Row style={{ alignItems: 'center' }}>
+				<Col sm={2}>
 					<Nav variant="pills" className="flex-column" style={{ textAlign: 'center' }}>
-						{/* {subject.data.map((work: string) => (
-							<Nav.Item key={`${subject.name}-works-item`}>
-								<Nav.Link eventKey={subject.eventkey}>{work}</Nav.Link>
+						{works.map(work => (
+							<Nav.Item key={`${work}-work`}>
+								<Nav.Link eventKey={`${work}-work`}>{work}</Nav.Link>
 							</Nav.Item>
-						))} */}
+						))}
 					</Nav>
 				</Col>
-				<SubjectWin />
+				<Col sm={10}>
+					<Tab.Content>
+						{works.map(work => (
+							<Tab.Pane eventKey={`${work}-work`} key={`${work}-work`}>
+								<SubjectWin />
+							</Tab.Pane>
+						))}
+					</Tab.Content>
+				</Col>
 			</Row>
 		</Tab.Container>
 	);
