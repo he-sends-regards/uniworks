d44730015e0313e5682e5031786725fb79d21da0
postscriptumno <postscriptum.no@gmail.com>
2021-01-21 16:39:20 +0200

    Написал базовую форму с bootstrap

--- client/src/components/form/form.tsx
+++ client/src/components/form/form.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
 
@@ -22,7 +23,7 @@ type PersonScore = {
 	password: string;
 };
 
-const Form: React.FC = () => {
+const AuthForm: React.FC = () => {
 	const { register, handleSubmit } = useForm<PersonScore>();
 	let { formType }: any = ({} = useParams());
 
@@ -31,38 +32,71 @@ const Form: React.FC = () => {
 	};
 
 	return (
-		<div className="row form-wrapper">
-			<form className="col s12 l12 form" onSubmit={handleSubmit(onSubmit)}>
-				<div className="row">
-					<h4 className="col s12">{`${formType.slice(0, 1).toUpperCase()}${formType.slice(1)}`}</h4>
-				</div>
+		<div className="form-wrapper">
+			<h2>{formType === 'login' ? `Вход в систему` : 'Регистрация'}</h2>
+			<Form className="form">
+				{formType === 'register' && (
+					<Form.Group>
+						<Form.Label>Имя</Form.Label>
+						<Form.Control type="text" placeholder="Введите имя" />
+					</Form.Group>
+				)}
 
-				{inputFields.map(({ id, type, placeholder }) => {
-					return (
-						<div className="row" key={`${id}-input`}>
-							<div className="input-field col s12">
-								<input
-									id={id}
-									name={id}
-									type={type}
-									className="validate"
-									placeholder={placeholder}
-									ref={register({ required: true })}
-								/>
-							</div>
-						</div>
-					);
-				})}
+				<Form.Group id="formBasicEmail">
+					<Form.Label>Email</Form.Label>
+					<Form.Control type="email" placeholder="Введите email" />
+					<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+				</Form.Group>
 
-				<div className="row">
-					<button className="btn waves-effect waves-light col s12" type="submit" name="action">
-						Submit
-						<i className="material-icons right">send</i>
-					</button>
-				</div>
-			</form>
+				<Form.Group id="formBasicPassword">
+					<Form.Label>Пароль</Form.Label>
+					<Form.Control type="password" placeholder="Введите пароль" />
+				</Form.Group>
+
+				{formType === 'login' && (
+					<Form.Group id="formBasicCheckbox">
+						<Form.Check type="checkbox" label="Остаться в системе" />
+					</Form.Group>
+				)}
+
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
 		</div>
+
+		// <div className="row form-wrapper">
+		// 	<form className="col s6 l12 form" onSubmit={handleSubmit(onSubmit)}>
+		// 		<div className="row">
+		// 			<h4 className="col s6">{`${formType.slice(0, 1).toUpperCase()}${formType.slice(1)}`}</h4>
+		// 		</div>
+
+		// 		{inputFields.map(({ id, type, placeholder }) => {
+		// 			return (
+		// 				<div className="row" key={`${id}-input`}>
+		// 					<div className="input-field col s12">
+		// 						<input
+		// 							id={id}
+		// 							name={id}
+		// 							type={type}
+		// 							className="validate"
+		// 							placeholder={placeholder}
+		// 							ref={register({ required: true })}
+		// 						/>
+		// 					</div>
+		// 				</div>
+		// 			);
+		// 		})}
+
+		// 		<div className="row">
+		// 			<button className="btn waves-effect waves-light col s6" type="submit" name="action">
+		// 				Submit
+		// 				<i className="material-icons right">send</i>
+		// 			</button>
+		// 		</div>
+		// 	</form>
+		// </div>
 	);
 };
 
-export default Form;
+export default AuthForm;
