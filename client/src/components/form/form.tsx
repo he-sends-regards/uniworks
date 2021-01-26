377fb7b09c1bfce8d750168d0c02074f52f2e1f2
postscriptumno <postscriptum.no@gmail.com>
2021-01-26 15:57:35 +0200

    Добавил проверку статуса авторизации с редиректом, если пользователь уже авторизован

--- client/src/components/form/form.tsx
+++ client/src/components/form/form.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Redirect, useParams } from 'react-router-dom';
+import { AuthorizationStatus } from '../../const';
 
 import './form.css';
 
@@ -9,98 +11,107 @@ interface IRefRegTypes {
 	[key: string]: string;
 }
 
-type FormDataTypes = {
-	[key: string]: string;
-};
-
 interface UrlParamTypes {
 	formType: string;
 }
 
-const AuthForm: React.FC = () => {
+type FormDataTypes = {
+	[key: string]: string;
+};
+
+interface IAuthFormProps {
+	authorizationStatus?: 'AUTH' | 'NO_AUTH';
+}
+
+const AuthForm: React.FC<IAuthFormProps> = ({ authorizationStatus }) => {
+	if (authorizationStatus === AuthorizationStatus.AUTH) {
+		alert('Вы уже авторизованы');
+		return <Redirect to="/account" />;
+	}
+
 	const { formType } = useParams<UrlParamTypes>();
 	const { register, handleSubmit, errors } = useForm();
 
 	const onSubmit = (data: FormDataTypes) => console.log(data);
 
 	return (
-		<>
-			<div className="form-wrapper">
-				<h2>{formType === 'login' ? `Вход в систему` : 'Регистрация'}</h2>
-				<Form className="form" onSubmit={handleSubmit(onSubmit)}>
-					{formType === 'register' && (
-						<Form.Group>
-							<Form.Label htmlFor="name">Имя</Form.Label>
-							<Form.Control
-								ref={register({ required: true, maxLength: 30, minLength: 2 })}
-								type="text"
-								id="name"
-								name="name"
-								aria-invalid={errors.name ? 'true' : 'false'}
-								placeholder="Введите имя"
-							/>
-							{errors.name && errors.name.type === 'required' && (
-								<span role="alert">This is required</span>
-							)}
-							{errors.name && errors.name.type === 'maxLength' && (
-								<span role="alert">Max length exceeded</span>
-							)}
-							{errors.name && errors.name.type === 'minLength' && (
-								<span role="alert">Min length exceeded</span>
-							)}
-						</Form.Group>
-					)}
-
-					<Form.Group id="formBasicEmail">
-						<Form.Label htmlFor="email">Email</Form.Label>
+		<div className="form-wrapper">
+			<h2>{formType === 'login' ? `Вход в систему` : 'Регистрация'}</h2>
+			<Form className="form" onSubmit={handleSubmit(onSubmit)}>
+				{formType === 'register' && (
+					<Form.Group>
+						<Form.Label htmlFor="name">Имя</Form.Label>
 						<Form.Control
-							ref={register({ required: true })}
-							name="email"
-							id="email"
-							type="email"
-							aria-invalid={errors.email ? 'true' : 'false'}
-							placeholder="Введите email"
+							ref={register({ required: true, maxLength: 30, minLength: 2 })}
+							type="text"
+							id="name"
+							name="name"
+							aria-invalid={errors.name ? 'true' : 'false'}
+							placeholder="Введите имя"
 						/>
-						{errors.email && errors.email.type === 'required' && <span role="alert">This is required</span>}
-						<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-					</Form.Group>
-
-					<Form.Group id="formBasicPassword">
-						<Form.Label htmlFor="password">Пароль</Form.Label>
-						<Form.Control
-							ref={register({ required: true, maxLength: 16, minLength: 7 })}
-							name="password"
-							id="password"
-							type="password"
-							placeholder="Введите пароль"
-						/>
-						{errors.password && errors.password.type === 'required' && (
-							<span role="alert">This is required</span>
-						)}
-						{errors.password && errors.password.type === 'maxLength' && (
+						{errors.name && errors.name.type === 'required' && <span role="alert">This is required</span>}
+						{errors.name && errors.name.type === 'maxLength' && (
 							<span role="alert">Max length exceeded</span>
 						)}
-						{errors.password && errors.password.type === 'minLength' && (
+						{errors.name && errors.name.type === 'minLength' && (
 							<span role="alert">Min length exceeded</span>
 						)}
 					</Form.Group>
+				)}
 
-					{formType === 'login' && (
-						<Form.Group id="formBasicCheckbox">
-							<Form.Check type="checkbox" label="Остаться в системе" />
-						</Form.Group>
+				<Form.Group id="formBasicEmail">
+					<Form.Label htmlFor="email">Email</Form.Label>
+					<Form.Control
+						ref={register({ required: true })}
+						name="email"
+						id="email"
+						type="email"
+						aria-invalid={errors.email ? 'true' : 'false'}
+						placeholder="Введите email"
+					/>
+					{errors.email && errors.email.type === 'required' && <span role="alert">This is required</span>}
+					<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+				</Form.Group>
+
+				<Form.Group id="formBasicPassword">
+					<Form.Label htmlFor="password">Пароль</Form.Label>
+					<Form.Control
+						ref={register({ required: true, maxLength: 16, minLength: 7 })}
+						name="password"
+						id="password"
+						type="password"
+						placeholder="Введите пароль"
+					/>
+					{errors.password && errors.password.type === 'required' && (
+						<span role="alert">This is required</span>
 					)}
+					{errors.password && errors.password.type === 'maxLength' && (
+						<span role="alert">Max length exceeded</span>
+					)}
+					{errors.password && errors.password.type === 'minLength' && (
+						<span role="alert">Min length exceeded</span>
+					)}
+				</Form.Group>
 
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
-				</Form>
-			</div>
-		</>
+				{formType === 'login' && (
+					<Form.Group id="formBasicCheckbox">
+						<Form.Check type="checkbox" label="Остаться в системе" />
+					</Form.Group>
+				)}
+
+				<Button variant="primary" type="submit">
+					Submit
+				</Button>
+			</Form>
+		</div>
 	);
 };
 
-export default AuthForm;
+const mapStateToProps = (state: any) => ({
+	authorizationStatus: state.authorizationStatus
+});
+
+export default connect(mapStateToProps)(AuthForm);
 
 const refRegisterAlerts: IRefRegTypes = {
 	required: 'This is required',
