110cd7a8d52ea4dca3d170eae8c0f19f0b805120
postscriptumno <postscriptum.no@gmail.com>
2021-01-28 18:03:05 +0200

    Добавил компонент превью работы

--- client/src/components/works/works.tsx
+++ client/src/components/works/works.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Filter from '../filter/filter';
+import WorksPreview from '../works-preview/works-preview';
 
 import './works.css';
 
@@ -7,6 +8,8 @@ const Works = () => {
 	return (
 		<div className="works-container">
 			<Filter />
+
+			<WorksPreview />
 		</div>
 	);
 };
