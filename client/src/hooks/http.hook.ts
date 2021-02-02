4af6c5e51b7e0f415512ec19bced272af4e4f685
postscriptumno <postscriptum.no@gmail.com>
2021-02-02 19:03:51 +0200

    Написал хук для работы с сервером

---
+++ client/src/hooks/http.hook.ts
@@ -0,0 +1,50 @@
+import { useCallback, useState } from 'react';
+
+interface IUseHttpReturn {
+	method: string;
+	body: any;
+	headers: object;
+}
+
+export const useHttp = () => {
+	const [loading, setLoading] = useState(false);
+	const [serverError, setServerError] = useState(null);
+
+	const request = useCallback(
+		async (url: string, method: string = 'GET', body: any = null, headers: any = {}): Promise<any> => {
+			setLoading(true);
+
+			try {
+				if (body) {
+					body = JSON.stringify(body);
+					headers['Content-Type'] = 'application/json';
+				}
+
+				const response = await fetch(url, { method, body, headers });
+				const data = await response.json();
+
+				if (!response.ok) {
+					throw new Error(data.message || 'При запросе что-то пошло не так');
+				}
+
+				setLoading(false);
+
+				return data;
+			} catch (err) {
+				setLoading(false);
+				setServerError(err.message);
+				throw err;
+			}
+		},
+		[]
+	);
+
+	const clearError = () => setServerError(null);
+
+	return {
+		loading,
+		request,
+		clearError,
+		serverError
+	};
+};
